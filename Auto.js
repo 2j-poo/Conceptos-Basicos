1effdf1cf0a13a13eb1be3a7ce358e169f3f16c4
20150853 CUEVAS ZEPEDA ALEJANDRO <20150853@TELEMATICA.UCOL.MX>
2019-02-08 15:36:39 -0600

    Segundo

--- Auto.js
+++ Auto.js
@@ -1,9 +1,41 @@
-class Auto {
-    constructor(marca, modelo, color, kilometraje){
-        this.marca = marca;
-        this.modelo = modelo;
-        this.color = color;
-        this.kilometraje = kilometraje;
-        this.estado = "Apagado";
+export default class Auto {
+    constructor(marca, modelo, color = "blanco", kilometraje = 0){
+        this._marca = marca;
+        this._modelo = modelo;
+        this._color = color;
+        this._kilometraje = kilometraje;
+        this._estado = "Apagado";
+    }
+
+    get color() {
+        return this._color; // Escritura
+    }
+
+    set color(color) {
+        this._color = color.toUpperCase(); // Lectura
+    }
+
+    mostrarEstado() {
+        console.log(`El auto ${this._marca} ${this._color} del año ${this._modelo} tiene un kilometraje de ${this._kilometraje} y esta ${this._estado}`);
+    }
+
+    encender() {
+        this._estado = "Encendido";
+        this.mostrarEstado();
+    }
+
+    apagar() {
+        this._estado = "Apagar";
+        this.mostrarEstado();
+    }
+
+    avanzar(velocidad, tiempo) {
+        if(this._estado === "Apagado") {
+            console.log("El auto no puede avanzar por que esta apagado");
+        } else {
+            let distancia =  velocidad * tiempo;
+            this._kilometraje = this._kilometraje + distancia;
+            this.mostrarEstado();
+        }
     }
 }
